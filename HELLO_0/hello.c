1af5d50f560472c42b762e5ff6e2c92e9e96f153
SIMONE010afk <simone.torrengo@edu-its.it>
2024-12-20 16:09:18 +0000

    Hello.c updating

--- HELLO_0/hello.c
+++ HELLO_0/hello.c
@@ -11,5 +11,22 @@ int main()
     // \n serve per spaziare dal messaggio sul terminale
     printf("hello world!!\n");
 
+    // variable of 32 chars
+    char name[32];
+
+    // ask for a name 
+    printf("Enter a name: ");
+
+    // scanf read the input of the user
+    // elenco cosa voglio leggere e cos'è che legge quindi caratteri (%s) = stringa  
+
+    scanf("%s", name);
+
+    // print the message with the name entered
+    // \t = tabulazione
+    printf("\n HEllo \t %s \n", name );
+
+
+
     return 0;
 }
