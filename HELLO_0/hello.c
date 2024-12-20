20dc24ee3e463a8680f78f80b01de430c5fa4727
SIMONE010afk <simone.torrengo@edu-its.it>
2024-12-20 15:44:27 +0000

    prima lezione creato hello.c

---
+++ HELLO_0/hello.c
@@ -0,0 +1,15 @@
+// #include fa l'inclusione di tutte le funzioni per input e output
+/*per commentare posso
+usare anche questo modo*/
+
+#include <stdio.h>
+
+// int ritorna un valore intero, se ritorna a zero va tutto bene
+
+int main() 
+{
+    // \n serve per spaziare dal messaggio sul terminale
+    printf("hello world!!\n");
+
+    return 0;
+}
