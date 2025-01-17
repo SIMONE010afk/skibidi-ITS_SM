b2a9a13130e1a25243cc5e87baf637516e5da062
SIMONE010afk <simone.torrengo@edu-its.it>
2025-01-17 16:54:48 +0000

    nuova es

---
+++ LEZ_MARCHESE_PROGRAMMAZIONE/Compiti/compito 2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define SOMMA(x,y) x+y
+#define SOTTRAZIONE(x,y) x-y
+#define MOLTIPLICAZIONE(x,y) x * y
+#define DIVISIONE(x,y) x / y
+#define MODULO(x,y) x%y
+#define NEW_LINE printf("\n");
+
+int main()
+{
+    int x, y;
+
+    printf("inserisci x: ");
+
+    scanf("%d", &x);
+
+    printf("inserisci y: ");
+
+    scanf("%d", &y);
+
+    printf("La somma dei 2 numeri è %d\n", SOMMA(x,y));
+
+    printf("La sottrazione dei 2 numeri è %d\n", SOTTRAZIONE(x,y));
+
+    printf("La moltiplicazione dei 2 numeri è %d\n", MOLTIPLICAZIONE(x,y));
+
+    double z = x, w = y, t;
+
+    printf("La divisione dei 2 numeri è %.2f\n", DIVISIONE(z,w));
+
+    printf("Il modulo dei 2 numeri è %d\n", MODULO(x,y));
+    
+    t = ((((SOMMA(x,y))+SOTTRAZIONE(x,y))+MOLTIPLICAZIONE(x,y))+DIVISIONE(z,w))+MODULO(x,y);
+
+    printf("Il risultato di tutte le operazioni precedenti è %.2f\n", t);
+
+    printf("Grazie per aver usato il nostro programma.");
+
+    NEW_LINE;
+    NEW_LINE;
+    return 0;
+
+}
