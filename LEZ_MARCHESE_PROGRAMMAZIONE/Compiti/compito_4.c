632a61336216c685ce67f0bc87cb24187681005d
SIMONE010afk <simone.torrengo@edu-its.it>
2025-01-21 15:20:27 +0000

    update

--- LEZ_MARCHESE_PROGRAMMAZIONE/Compiti/compito_4.c
+++ LEZ_MARCHESE_PROGRAMMAZIONE/Compiti/compito_4.c
@@ -1,3 +1,11 @@
+/*Scrivere un programma in C che:
+
+Legga da tastiera il giorno della settimana (come stringa) e la spesa effettuata in quel giorno.
+Usi una macro per calcolare l'IVA (22%) sulla spesa.
+Usi una struttura di controllo if-else per distinguere i giorni feriali da quelli festivi.
+Stampi il totale delle spese con e senza IVA, specificando se il giorno è feriale o festivo.
+*/
+
 #include <stdio.h>
 #include <string.h>
 
